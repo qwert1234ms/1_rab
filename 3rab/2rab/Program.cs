942c19be91e8f2781a480b384903cb8c6ead51fa
qwert1234ms <80520654+qwert1234ms@users.noreply.github.com>
2021-09-08 20:08:28 +0300

    Add files via upload

---
+++ 3rab/2rab/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _2rab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество символов для идентификартора пользователя");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите идентификатор пользователя");
+            string x = Console.ReadLine();
+            if (x.Length < N)
+            {
+                Console.WriteLine("Вы должны ввести идентификатор длиной не менее " + N + " символов");
+            }
+            else if (x.Length > N)
+            {
+                Console.WriteLine("Вы должны ввести идентификатор длиной не более " + N + " символов");
+            }
+            else
+            {
+                Console.WriteLine("Идентификатор пользователя: " + x);
+            }
+
+            int M = 10;
+
+            Console.WriteLine("Количество символов пароля: " + M);
+
+            int Q = N % 5;
+            Console.WriteLine(Q);
+
+            string abc = "qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlIzZxXcCvVbBnNmM"; //набор символов
+            string result = "";
+
+            Random rnd = new Random();
+            int lng = abc.Length;
+            for (int i = 0; i < M; i++)
+                result += abc[rnd.Next(lng)];
+            Console.WriteLine(result);
+        }
+    }
+}
