8b79918b8209f352a7da2d3fc8c4547144705142
qwert1234ms <80520654+qwert1234ms@users.noreply.github.com>
2021-09-08 20:25:23 +0300

    Add files via upload

---
+++ 4rab/4rab/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _4rab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\input.txt");
+            StreamReader sr1 = new StreamReader(@"C:\dictionary.txt");
+            string text1 = sr.ReadToEnd();
+            string text2 = sr1.ReadToEnd();
+            string[] words = SplitWords(text1).Intersect(SplitWords(text2), StringComparer.CurrentCultureIgnoreCase).ToArray();
+            Console.WriteLine("Количество совпадающих слов: {0}", words.Length);
+            Console.ReadKey();
+        }
+        static string[] SplitWords(string input)
+        {
+            return input.Split(new[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
